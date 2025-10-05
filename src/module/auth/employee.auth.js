4762a641e50867295ba391de8ef3b90ec0c637f3
Mustafiz-xponent <mustafiz.xponent@gmail.com>
2025-10-05 09:55:11 +0600

    AUthentication Implemented

--- src/module/auth/employee.auth.js
+++ src/module/auth/employee.auth.js
@@ -1,48 +1,68 @@
 import prisma from "../../prisma-client/prismaClient.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 // SignUp user
 const signUpUser = async (req, res) => {
-  const { email, password, role } = req.body;
+  try {
+    const { email, password, role } = req.body;
 
-  if (!email || !password || !role) {
-    return res.status(400).json({ error: "Name and password are required" });
+    if (!email || !password || !role) {
+      return res.status(400).json({ error: "Name and password are required" });
+    }
+
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
+    const newUser = await prisma.user.create({
+      data: {
+        email,
+        passwordHash,
+        role,
+      },
+    });
+
+    return res
+      .status(201)
+      .json({ message: "User created successfully", userId: newUser.id });
+  } catch (err) {
+    return res.status(500).json({ error: "Internal server error" });
   }
-
-  const saltRounds = 10;
-  const passwordHash = await bcrypt.hash(password, saltRounds);
-  const newUser = await prisma.user.create({
-    data: {
-      email,
-      passwordHash,
-      role,
-    },
-  });
-
-  return res
-    .status(201)
-    .json({ message: "User created successfully", userId: newUser.id });
 };
 
 // Login User
 const loginUser = async (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    return res.status(400).json({ error: "Email and password are required" });
-  }
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ error: "Email and password are required" });
+    }
 
-  const user = await prisma.user.findUnique({
-    where: { email },
-  });
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
 
-  if (!user) {
-    return res.status(401).json({ error: "Invalid email or password" });
-  }
+    if (!user) {
+      return res
+        .status(401)
+        .json({ error: "There is no account related to this email" });
+    }
 
-  // Here you would typically compare the hashed passwords
-  const passwordMatch = await bcrypt.compare(password, user.passwordHash);
-  if (!passwordMatch) {
-    return res.status(401).json({ error: "Passwords do not matched" });
+    // Here you would typically compare the hashed passwords
+    const passwordMatch = await bcrypt.compare(password, user.passwordHash);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: "Passwords do not matched" });
+    }
+
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    return res.status(200).json({
+      message: "User Logged in suceesfully",
+      data: { token, userId: user.id, role: user.role },
+    });
+  } catch (err) {
+    return res.status(500).json({ error: "Internal server error" });
   }
 };
 
@@ -68,4 +88,4 @@ const loginUser = async (req, res) => {
 //   console.log(name);
 // };
 
-export { signUpUser };
+export { signUpUser, loginUser };
--- src/module/auth/employee.routes.js
+++ src/module/auth/employee.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { signUpUser } from "./employee.auth.js";
+import { signUpUser, loginUser } from "./employee.auth.js";
 
 const router = Router();
 
-// Route to add a new employee
+// Route to Signup and Login a new employee
 router.post("/sign-up", signUpUser);
+router.post("/login", loginUser);
 
 export default router;
