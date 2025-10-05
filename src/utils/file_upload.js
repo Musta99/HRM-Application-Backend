8c42693855fa7f3ef73771f86db30b9fe56c6855
Mustafiz-xponent <mustafiz.xponent@gmail.com>
2025-10-05 12:35:03 +0600

    multer and cloudinary configation

---
+++ src/utils/file_upload.js
@@ -0,0 +1,35 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Cloudinary Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadToCloudinary = async (filePath) => {
+  try {
+    if (!filePath) {
+      return null;
+    }
+    // upload the file to cloudinary
+    const response = await cloudinary.uploader.upload(filePath, {
+      resource_type: "auto",
+      folder: "hrm_application_profileImages",
+    });
+    console.log("Printing the response", response);
+    // file has been successfully uploaded,
+    console.log("File uploaded to Cloudinary successfully");
+    // delete the file from local server
+    fs.unlinkSync(filePath);
+    console.log("File deleted from local server");
+    return response;
+  } catch (err) {
+    fs.unlinkSync(filePath);
+    console.error("Error uploading file to Cloudinary:", err);
+    return null;
+  }
+};
+
+export { uploadToCloudinary };
