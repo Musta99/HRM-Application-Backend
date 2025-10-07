bcae216dbc5bba0a583d6ac9884ec4b200f2315d
Mustafiz-xponent <mustafiz.xponent@gmail.com>
2025-10-07 16:37:18 +0600

    Fetch user by id implemneted

--- src/module/user/users.controller.js
+++ src/module/user/users.controller.js
@@ -2,18 +2,32 @@ import prisma from "../../prisma-client/prismaClient.js";
 
 const getAllUsers = async (req, res) => {
   try {
+    const { id } = req.params;
+
+    if (id) {
+      const user = await prisma.user.findUnique({
+        where: { id },
+      });
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      return res.status(200).json({
+        message: "User fetched successfully",
+        data: user,
+      });
+    }
+
     const users = await prisma.user.findMany();
     console.log(users);
-     res.status(200).json({
+    res.status(200).json({
       message: "Users fetched successfully",
       data: users,
     });
   } catch (err) {
     console.log("Error fetching users:", err);
-      res.status(500).json({ err });
+    res.status(500).json({ err });
   }
 };
 
 export { getAllUsers };
-
-console.log("Helo")
--- src/module/user/users.routes.js
+++ src/module/user/users.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { getAllUsers } from "./users.controller.js";
+import { authMiddleware } from "../../middleware/authMiddleware.js";
 
 const router = Router();
 
 // Route to get all users
 router.get("/", getAllUsers);
+router.get("/:id", authMiddleware, getAllUsers);
 
 export default router;
