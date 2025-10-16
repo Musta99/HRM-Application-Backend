555b37f36c3b9dc59e07e2bb39e6bb170f7e34ca
Mustafiz-xponent <mustafiz.xponent@gmail.com>
2025-10-16 15:31:47 +0600

    Create Task Implemented

--- src/module/task/task.controller.js
+++ src/module/task/task.controller.js
@@ -6,14 +6,60 @@ const createNewTask = async (req, res) => {
     const { title, description, status, priority, startDate, endDate } =
       req.body;
 
-    // if (!title || !description || !startDate) {
-    //   return res.status(400).json({
-    //     message: "Please prove all the input fields",
-    //   });
-    // }
+    if (!title || !description || !startDate) {
+      return res.status(400).json({
+        message: "Please prove all the input fields",
+      });
+    }
 
     const userId = req.user.id;
-    console.log(userId);
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!user.reportingBoss) {
+      return res.status(400).json({
+        message: "Please Add your reporting boss first",
+      });
+    }
+
+    const task = await prisma.taskManagement.create({
+      data: {
+        title,
+        description,
+        status,
+        priority,
+        startDate: new Date(startDate),
+        employee: {
+          connect: {
+            id: userId,
+          },
+        },
+        manager: {
+          connect: {
+            id: user.reportingBoss,
+          },
+        },
+      },
+
+      include: {
+        employee: {
+          select: { name: true, email: true, id: true },
+        },
+
+        manager: {
+          select: { name: true, email: true, id: true },
+        },
+      },
+    });
+
+    return res.status(200).json({
+      message: "Your task has been successfully added",
+      data: task,
+    });
   } catch (err) {
     console.log("Error creating leave request:", err);
     return res.status(500).json({ error: "Internal server error" });
--- src/module/task/task.routes.js
+++ src/module/task/task.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { createNewTask } from "./task.controller.js";
+import { authMiddleware } from "../../middleware/authMiddleware.js";
 
 const router = Router();
 
-router.post("/", createNewTask);
+router.post("/", authMiddleware, createNewTask);
 
 export default router;
