7b352101ef28ce48506c3974b47612018194082a
Mustafiz-xponent <mustafiz.xponent@gmail.com>
2025-10-27 12:28:56 +0600

    document updated controller implemented

--- src/module/documents/documents.controller.js
+++ src/module/documents/documents.controller.js
@@ -29,12 +29,10 @@ const uploadDocuments = async (req, res) => {
       });
     }
     const docPath = req.files?.document?.[0]?.path;
-
     if (docPath) {
       const { size } = fs.statSync(docPath);
       docSize = (size / 1024).toFixed(2);
       docFormat = docPath.split(".").pop().split("-")[0];
-
       const uploadResult = await uploadToCloudinary(docPath);
       document = uploadResult.url;
     }
@@ -52,10 +50,41 @@ const uploadDocuments = async (req, res) => {
     });
 
     return res.status(201).json({
+      success: true,
       message: "Document has been uploaded successfully",
     });
+  } catch (err) {
+    console.log("Some Error occured", err);
+    return res.status(500).json({
+      message: `Some Error occured: ${err}`,
+    });
+  }
+};
 
-    console.log(user);
+// Update any document information
+const updateDocInfo = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const { docId } = req.params;
+    const { title, description } = req.body;
+    const updatedData = {
+      title,
+      description,
+    };
+
+    // if this documents belongs to you, only then you can update it
+    const document = await prisma.documents.update({
+      where: {
+        userId: new ObjectId(userId),
+        id: new ObjectId(docId),
+      },
+      data: updatedData,
+    });
+
+    return res.status(200).json({
+      message: "document updated successfully",
+      data: document,
+    });
   } catch (err) {
     console.log("Some Error occured", err);
     return res.status(500).json({
